3f69cfc7bc63e7bd8c9fd0521705e1edd3cd3490
Clint Ast <clintast@gmail.com>
2017-02-19 04:55:44 -0600

    added a posts_index

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,20 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
 
-<Route path="/" component={App} />
+const Greeting1 = () => {
+  return <div>Hey there!</div>
+}
+const Greeting2 = () => {
+  return <div>Ola alla!</div>
+}
+const Greeting3 = () => {
+  return <div>Good Morning!</div>
+}
+
+export default (
+  <Route path="/" component={App} >
+    <Route path="greet1" component={Greeting1} />
+    <Route path="greet2" component={Greeting2} />
+    <Route path="greet3" component={Greeting3} />
+  </Route>
+);
