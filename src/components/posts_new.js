70bf9eb577f8c129678217a472c1071627284c95
Clint Ast <clintast@gmail.com>
2017-02-19 10:10:42 -0600

    added missing field validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -13,22 +13,48 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : '' }
+          </div>
         </div>
         <div className="form-group">
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : '' }
+          </div>
         </div>
         <div className="form-group">
           <label>Content</label>
           <textarea type="text" className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : '' }
+          </div>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
 }
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter category';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content';
+  }
+
+  return errors;
+}
+
 // reduxForm: 1st argument is form config, 2nd is mapStateToProps, 3rd is MapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, {createPost})(PostsNew);
