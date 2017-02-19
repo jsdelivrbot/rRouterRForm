7d53c636959b4d575e2838b2d766f271adcb3880
Clint Ast <clintast@gmail.com>
2017-02-19 06:34:01 -0600

    added component will mount to fetch Posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+
+export default connect(null, {fetchPosts})(PostsIndex);
+
+/*
+above is an easier way of doing the below
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchPosts}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
+*/
